dc88261500d3619b9edbd1f2ce4c8fbf6b7adc99
brais421520 <braisravina@rocketmail.com>
2019-11-04 16:16:43 +0000

    Create shell.c

---
+++ shell.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <string.h> 
+#include "mymemory.h"
+
+int main()
+{
+    printf ( "shell> start\n"); initialize() ;
+        printmemory() ;
+        printsegmenttable() ;
+        printf ( "shell> end\n");
+    return 0; 
+    
+}
